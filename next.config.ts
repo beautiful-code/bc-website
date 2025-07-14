f3ad2e8b6630dfef27ef8aba15be30eab9003031
Ravi Bhim <ravi.bhim@yahoo.com>
2025-07-14 09:44:49 +0530

    Revert "Experimental fix for HMR issue"
    This reverts commit f7275f4bbc7b13a06bc9374754de807c5b5879b0.

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Improve development experience
-  experimental: {
-    // Reduce file system watchers conflicts
-    turbo: {
-      resolveAlias: {
-        // Reduce resolution conflicts
-        "@": "./src",
-      },
-    },
-  },
-
-  // Optimize webpack for better file handling
-  webpack: (config, { dev }) => {
-    if (dev) {
-      // Reduce file system polling conflicts
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-        ignored: ["**/node_modules/**", "**/.next/**"],
-      };
-    }
-    return config;
-  },
-
-  // Optimize dev server
-  devIndicators: {
-    buildActivity: false, // Reduces file system activity
-  },
+  /* config options here */
 };
 
 export default nextConfig;
